f2353c820bd2600456cc97b19d3485cf8e9c207e
Tyler Marshall <tyler@dovetaildigital.ca>
2023-04-03 11:48:16 -0700

    Adds more mobs, and directional movement toward player.

--- Monster/monster.gd
+++ Monster/monster.gd
@@ -1,15 +1,30 @@
 extends Area2D
 
 @export var health = 20
+@onready var player = $"../Player"
+@onready var healthBar = $HealthBar
 
 signal damaged
+
 var damageTaken
 var totalSize = 100
-
-func _ready():
-	var healthBar = $HealthBar
 	
 func _on_body_entered(body):
+	takeDamage(body)
+
+func _on_damaged():
+	var xRemove = totalSize / damageTaken
+	$HealthBar/Green.size.x -= xRemove
+
+func _physics_process(delta):
+	moveTowardPlayer(delta)
+
+func moveTowardPlayer(delta):
+	var direction = Vector2(player.position - self.position)
+	var moveSpeed = direction * delta * 0.04
+	position += moveSpeed
+
+func takeDamage(body):
 	damageTaken = body.damage
 	health -= damageTaken
 	body.queue_free()
@@ -17,8 +32,3 @@ func _on_body_entered(body):
 	print("Hit, health: ", health)
 	if (health <= 0):
 		queue_free()
-
-
-func _on_damaged():
-	var xRemove = totalSize / damageTaken
-	$HealthBar/Green.size.x -= xRemove
