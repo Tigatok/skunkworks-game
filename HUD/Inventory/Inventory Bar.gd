a4c0ae90f240ff25f87c99d99be8b2ee1ba986da
Tyler Marshall <tyler@dovetaildigital.ca>
2023-04-04 18:32:52 -0700

    Adds inventories, need to ensure to remove the non-current ones

--- HUD/Inventory/Inventory Bar.gd
+++ HUD/Inventory/Inventory Bar.gd
@@ -5,19 +5,27 @@ signal update_active_item
 var itemsInInventory = []
 var activeItem
 
-func _process(delta):
-	if (Input.is_action_just_pressed("1")):
-		# Highlight selected square.
-		if (0 < itemsInInventory.size()):
-			print("Item in 0 index")
-			# Highlight first inventory space
-			# set active item to 0
-			print(itemsInInventory[0].name)
-#	if (Input.is_action_just_pressed("2")):
-#		print("2")
-#		if (1 < itemsInInventory.size()):
-#			print("Item 1")
+const KEY_INVENTORY_1 = "1"
+const KEY_INVENTORY_2 = "2"
+const KEY_INVENTORY_3 = "3"
 
+func _process(delta):
+	if (Input.is_action_just_pressed(KEY_INVENTORY_1)):
+		set_active_inventory_item(0)
+	if (Input.is_action_just_pressed(KEY_INVENTORY_2)):
+		set_active_inventory_item(1)
+	if (Input.is_action_just_pressed(KEY_INVENTORY_3)):
+		set_active_inventory_item(2)
+
+func set_active_inventory_item(index):
+	var inventory_items = get_tree().get_nodes_in_group('inventory_items')
+	if index < inventory_items.size():
+		activeItem = index
+		inventory_items[index].setAsActive(true)
+		for item in inventory_items:
+			print("self", self, " item:", item)
+			item.emit_signal('deactivate')
+		
 func _on_hud_item_added(item):
 	addItemToInventoryArray(item)
 	addItemToInventoryBar(item)
--- HUD/Inventory/InventoryItem/InventoryItem.gd
+++ HUD/Inventory/InventoryItem/InventoryItem.gd
@@ -1,3 +1,15 @@
 extends Sprite2D
 
 var isActive = false
+signal deactivate
+
+func setAsActive(active):
+	isActive = active
+	print("Is active?", active)
+	if (active == true && !isActive):
+		isActive = true
+		$ActiveLabel.position = self.offset
+		$ActiveLabel.text = 'Active'
+
+func _on_deactivate():
+	setAsActive(false)
