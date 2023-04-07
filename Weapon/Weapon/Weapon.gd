04e16913b237cfce5210f6bca807ed43c92d2ed9
Tyler Marshall <tyler@dovetaildigital.ca>
2023-04-07 10:10:51 -0700

    Clean up inventory

--- Weapon/Weapon/Weapon.gd
+++ Weapon/Weapon/Weapon.gd
@@ -3,9 +3,13 @@ extends Node2D
 const ItemType = Item.ItemType
 
 @onready var icon = preload("res://Weapon/weapon.png")
-@onready var item = Item.new(0000, 'Cannon', icon, ItemType.EQUIPMENT)
+@export var item_id: int
+@export var item_name: String
+@export var item_icon: Texture
+@export var item_type: ItemType
 
 func _ready():
+	var item = Item.new(item_id, item_name, item_icon, item_type)
 	var item_pickup_instance = $ItemPickup
 	item_pickup_instance.item = item
 	item_pickup_instance.get_node("ItemIcon").texture = icon
