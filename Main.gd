d0a558a19ef93bcd6877dcda49f8f0f5bc1ccced
Tyler Marshall <tyler@dovetaildigital.ca>
2023-04-19 05:55:03 -0700

    Update

--- Main.gd
+++ Main.gd
@@ -2,6 +2,7 @@ extends Node
 
 @onready var player = $Player
 @onready var inventory_interface:InventoryInterface = $UI/InventoryInterface
+@onready var player_inventory = $PlayerInventory
 
 func _ready() ->void:
 	inventory_interface.set_player_inventory_data(player.inventory_data)
