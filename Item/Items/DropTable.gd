e2ec61133b252e17542599ba4835f37b2a04e346
Tyler Marshall <tyler@dovetaildigital.ca>
2023-04-20 17:05:46 -0700

    Adds drop table proper functionality

--- Item/Items/DropTable.gd
+++ Item/Items/DropTable.gd
@@ -5,9 +5,7 @@ class_name DropTable extends Resource
 func add_item(monster_id:String, item: ItemData, drop_rate: float) -> void:
 	drop_data[monster_id] = {'item': item, 'drop_rate': drop_rate}
 
-func get_drop() -> Array:
-	var dropped_items = []
-	for item_id in drop_data:
-		if randf() <= drop_data[item_id]:
-			dropped_items.append(item_id)
-	return dropped_items
+func get_drop(monster_id:String):
+	for item_id in drop_data[monster_id]:
+		if randf() <= drop_data[monster_id]['drop_rate']:
+			return drop_data[monster_id]['item']
--- Item/Items/ItemsManager.gd
+++ Item/Items/ItemsManager.gd
@@ -4,14 +4,15 @@ extends Node
 @onready var item_pickup = preload("res://Item/ItemPickup/ItemPickup.tscn")
 
 func _ready():
-	drop_table.add_item('blob', preload("res://Item/Items/gray_cannon.tres"), 0.5)
+	drop_table.add_item('blob', preload("res://Item/Items/red_cannon.tres"), 0.05)
 
 func _on_monster_died(monster)->void:
-	#todo remove.
-	var item_to_drop = drop_table.drop_data['blob']
+	var item_to_drop = drop_table.get_drop(monster.monster_id)
+	if not item_to_drop:
+		return
 	var item_pickup_instance:ItemPickup = item_pickup.instantiate()
 	var slot_data = SlotData.new()
-	slot_data.item_data = item_to_drop.item
+	slot_data.item_data = item_to_drop
 	item_pickup_instance.slot_data = slot_data
 	item_pickup_instance.position = monster.position
 	monster.get_node('/root/Main').add_child(item_pickup_instance)
